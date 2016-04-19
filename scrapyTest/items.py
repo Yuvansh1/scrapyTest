8ce37e73fb469c8801c767f7704da4c1a32b119d
Pawneet Singh <pawneet.dev@gmail.com>
2016-04-19 16:21:16 +0530

    Test scraper

--- scrapyTest/items.py
+++ scrapyTest/items.py
@@ -1,11 +1,19 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/topics/items.html
-
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
 from scrapy.item import Item, Field
 
 class ScrapytestItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
+	name = 'Test'
+	allowed_domains = ["craigslist.org"]
+	start_urls = ["http://sfbay.craigslist.org/search/npo"]
+	title = Field()
+	link = Field()
     pass
+
+	def parse(self, response):
+		hxs = HtmlXPathSelector(response)
+		titles = hxs.select("//span[@class='p1']")
+		for title in titles:
+			title = titles.select("a/text()").extract()
+			link = titles.select("a/@href").extract()
+			print title, link
